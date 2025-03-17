89f8b1887bb36572dea21a689320a14c337842ba
Momona-Wada <144402784+Momona-Wada@users.noreply.github.com>
2025-03-17 00:23:16 -0700

    debug

--- frontend/app/lib/userDashboard.ts
+++ frontend/app/lib/userDashboard.ts
@@ -1,26 +1,27 @@
-import axios from "axios"
+import "server-only";
 
 const API_BASE_URL = "https://bcit-anthony-sh-s.com/lumisenseai/api/v1";
 
-interface User {
-    user_id: number,
-    email: string,
-    username: string,
-    is_admin: boolean
+export interface User {
+    user_id: number;
+    email: string;
+    username: string;
+    is_admin: boolean;
 }
 
 export const fetchUser = async (): Promise<User> => {
+    const response = await fetch(`${API_BASE_URL}/users/me`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+        "Content-Type": "application/json",
+        },
+    });
 
-    try {
-        const response = await axios.get(`${API_BASE_URL}/users/me`, {
-            withCredentials: true,
-            
-        });
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.detail || "Failed to fetch user. Please check your credentials.");
+    }
 
-        return response.data
-        
-    } catch (error) {
-        console.error("Error fetching user:", error);
-        throw error
-    }  
-}
+    return response.json();
+};
