c4a627f6e7a5b8312fbcfcaa600f6529c1362876
Anthony Shin <shinn92dev@gmail.com>
2025-03-25 02:48:21 -0700

    feat: implement selected device card in lumisenseai page

--- app/routes/api/lumisenseai.tsx
+++ app/routes/api/lumisenseai.tsx
@@ -2,7 +2,7 @@ import APIKeyCheck from "components/APIKeyCheckForm";
 import { FaRegLightbulb } from "react-icons/fa";
 import { LuLampDesk } from "react-icons/lu";
 import { useNavigate } from "react-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
     Card,
     CardContent,
@@ -17,11 +17,46 @@ import "./animation.css";
 const LumiSenseAI = () => {
     const navigate = useNavigate();
     const [isKeyChecked, setIsKeyChecked] = useState(true);
+    const [selectedDevice, setSelectedDevice] = useState<any>(null);
+
+    useEffect(() => {
+        const storedDevice = localStorage.getItem("selectedDevice");
+        if (storedDevice) {
+            setSelectedDevice(JSON.parse(storedDevice));
+        }
+    }, []);
 
     if (!isKeyChecked) return <APIKeyCheck />;
     return (
         <div className="w-full min-h-screen flex flex-col max-h-screen">
             <h1 className="text-3xl font-bold text-center">Lumi Sense AI</h1>
+            {selectedDevice && (
+                <div className="w-full md:px-10 flex flex-col justify-center items-center mt-10 gap-5">
+                    <h2>Currently Selected: </h2>
+                    <div className="w-full">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>{`${selectedDevice.deviceName} (${selectedDevice.model})`}</CardTitle>
+                                <CardDescription>{`${selectedDevice.device}`}</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <div>
+                                    <div className="flex max-w-lg gap-2">
+                                        <p>Supported: </p>
+                                        <p>{`${selectedDevice.controllable ? "YES" : "NO"}`}</p>
+                                    </div>
+                                    <div className="flex max-w-lg gap-2">
+                                        <p>Possible Action: </p>
+                                        {selectedDevice.supportCmds.map((cmd: any, idx: number) => {
+                                            return <p key={`${cmd}_${idx}`}>{cmd}</p>;
+                                        })}
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
+            )}
             <div className="w-full grid md:grid-cols-2 gap-10 gap-y-5 md:px-10 mt-35">
                 <Card
                     className="hover-click-animation"
