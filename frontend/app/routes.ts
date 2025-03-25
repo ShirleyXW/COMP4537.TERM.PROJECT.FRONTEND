aead88e41d5d1a0a08ab80e4469f0333e9d9174a
Anthony Shin <shinn92dev@gmail.com>
2025-03-25 09:26:21 -0700

    refactor: update routes.ts

---
+++ frontend/app/routes.ts
@@ -0,0 +1,12 @@
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
+
+export default [
+    index("routes/home.tsx"),
+    route("register", "routes/register.tsx"),
+    route("info", "routes/info.tsx"),
+    route("userDashboard", "routes/userDashboard.tsx"),
+    route("login", "routes/login.tsx"),
+    route("lumisenseai", "routes/api/lumisenseai.tsx"),
+    route("lumisenseai/connect", "routes/api/connect.tsx"),
+    route("lumisenseai/control", "routes/api/control.tsx"),
+] satisfies RouteConfig;
