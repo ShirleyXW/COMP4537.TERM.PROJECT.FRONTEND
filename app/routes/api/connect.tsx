3e23e097fd9fde4b0df1543936085c95e26c0abd
Anthony Shin <shinn92dev@gmail.com>
2025-03-25 02:17:48 -0700

    feat: implement device card

--- app/routes/api/connect.tsx
+++ app/routes/api/connect.tsx
@@ -2,7 +2,16 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import { Button } from "~/components/ui/button";
-
+import { Separator } from "@/components/ui/separator";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import "./animation.css";
 const mockDeviceData = [
     {
         device: "F0:72:D6:94:C1:86:73:2A",
@@ -37,9 +46,16 @@ const connect = () => {
     const [isAPIVerified, setIsAPIVerified] = useState(false);
     const [APIKey, setAPIKey] = useState("");
     const [devices, setDevices] = useState(null);
-    const handleSubmit = () => {
+    const [selectedDevice, setSelectedDevice] = useState<any>(null);
+    const [isEmptyAPIKey, setIsEmptyAPIKey] = useState(false);
+    const handleSubmit = (e: any) => {
+        if (APIKey.length <= 0) {
+            setIsEmptyAPIKey(true);
+            return;
+        }
         setIsAPIVerified(true);
         setAPIKey(APIKey);
+        setIsEmptyAPIKey(false);
     };
     const handleChange = () => {
         setIsAPIVerified(false);
@@ -51,27 +67,66 @@ const connect = () => {
                 {/* <h1 className="text-3xl font-bold">
                     {isAPIVerified ? "Your API Key registered" : "Enter your device API key"}
                 </h1> */}
-                <div className="grid w-full max-w-lg items-center gap-1.5">
+                <div className="grid w-full items-center gap-1.5">
                     <Label htmlFor="api-key">
                         {isAPIVerified ? "Your API Key registered" : "Enter your device API key"}
                     </Label>
-                    <div className="flex gap-5">
-                        {isAPIVerified ? (
-                            <Input value={APIKey} disabled />
-                        ) : (
-                            <Input
-                                id="api-key"
-                                type="text"
-                                placeholder="enter your api key here.."
-                                required
-                                value={APIKey}
-                                onChange={(e) => setAPIKey(e.target.value)}
-                            />
-                        )}
+                    <div className="flex gap-5 w-full">
+                        <div className="flex flex-col flex-grow gap-3">
+                            {isAPIVerified ? (
+                                <Input value={APIKey} disabled />
+                            ) : (
+                                <Input
+                                    id="api-key"
+                                    type="text"
+                                    placeholder="enter your api key here.."
+                                    required
+                                    value={APIKey}
+                                    onChange={(e) => setAPIKey(e.target.value)}
+                                />
+                            )}
+                            {isEmptyAPIKey && (
+                                <p className="text-sm text-red-500 ml-1">API Key is required</p>
+                            )}
+                        </div>
                         <Button onClick={isAPIVerified ? handleChange : handleSubmit}>
                             {isAPIVerified ? "Change" : "Submit"}
                         </Button>
                     </div>
+                </div>
+                <Separator />
+                <h2 className="text-2xl font-bold">
+                    Select One of Your Device That You Want to Control
+                </h2>
+                <div className="mt-7 gap-5 grid md:grid-cols-2">
+                    {mockDeviceData.map((device, idx) => {
+                        return (
+                            <Card
+                                key={idx}
+                                className="hover-click-animation"
+                                onClick={() => setSelectedDevice(device)}
+                            >
+                                <CardHeader>
+                                    <CardTitle>{`${device.deviceName} (${device.model})`}</CardTitle>
+                                    <CardDescription>{`${device.device}`}</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <div>
+                                        <div className="flex max-w-lg gap-2">
+                                            <p>Supported: </p>
+                                            <p>{`${device.controllable ? "YES" : "NO"}`}</p>
+                                        </div>
+                                        <div className="flex max-w-lg gap-2">
+                                            <p>Possible Action: </p>
+                                            {device.supportCmds.map((cmd, idx) => {
+                                                return <p key={`${cmd}_${idx}`}>{cmd}</p>;
+                                            })}
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        );
+                    })}
                 </div>
             </div>
         </div>
