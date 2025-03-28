1382db5614ea8799d181e9d9467872e7f4e0bba5
ShirleyXW <shirley.wang.xl@outlook.com>
2025-03-28 15:10:12 -0700

    refactor: record keyName

--- frontend/components/APIGenerate.tsx
+++ frontend/components/APIGenerate.tsx
@@ -11,6 +11,7 @@ import { generateApiKey } from "@/lib/api";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import type { APITable } from "~/routes/userDashboard";
+import { useState } from "react";
 
 interface APIGenerateProps {
     userId: number;
@@ -18,13 +19,15 @@ interface APIGenerateProps {
   }
 
 export function APIGenerate ({userId, setApiKeys}: APIGenerateProps) {
+    const [keyName, setKeyName] = useState<string>("");
 
     const generateKey = async () => {
         try {
-            const result = await generateApiKey(userId);
+            const result = await generateApiKey(userId, keyName);
             if (result?.success) {
                 const updatedData = {
                     key: result.key,
+                    key_name: keyName,
                     status: "active" as "active",
                     action: {
                         toggleStatus: "Toggle Status",
@@ -51,7 +54,7 @@ export function APIGenerate ({userId, setApiKeys}: APIGenerateProps) {
                 <TableBody>
                     <TableRow className="hover:bg-transparent">
                         <TableCell>
-                            <Input placeholder="Enter Key Name" />
+                            <Input placeholder="Enter Key Name" value={keyName} onChange={(e) => setKeyName(e.target.value)}/>
                         </TableCell>
                         <TableCell>
                             <Button className="bg-gradient-to-br from-blue-500 to-blue-300 rounded w-20 h-8 hover:from-blue-900 hover:to-blue-400" onClick={()=>generateKey()}>Generate</Button>
