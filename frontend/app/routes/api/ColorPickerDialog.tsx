73dceed7b7f9be396d5099bbafc565d6fc6d4e82
Anthony Shin <shinn92dev@gmail.com>
2025-03-30 02:37:12 -0700

    feat: implement api logic to color picker

--- frontend/app/routes/api/ColorPickerDialog.tsx
+++ frontend/app/routes/api/ColorPickerDialog.tsx
@@ -11,12 +11,57 @@ import {
 } from "@/components/ui/alert-dialog";
 import EmotionCard from "./EmotionCard";
 import { use, useState } from "react";
+import { API_BASE_URL } from "~/lib/api";
+import LoadingSpinner from "components/LoadingSpinner";
 
 import { HexColorPicker } from "react-colorful";
+const hexToRgb = (hex: string) => {
+    hex = hex.replace(/^#/, "");
 
-const ColorPickerDialog = () => {
+    if (hex.length === 3) {
+        hex = hex
+            .split("")
+            .map((char) => char + char)
+            .join("");
+    }
+
+    const bigint = parseInt(hex, 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+
+    return { r, g, b };
+};
+
+interface ColorPickerDialogProps {
+    goveeKey: string;
+    device: any;
+}
+
+const ColorPickerDialog: React.FC<ColorPickerDialogProps> = ({ goveeKey, device }) => {
     const [color, setColor] = useState("#ff0000");
-
+    const [isLoading, setIsLoading] = useState(false);
+    console.log(goveeKey, device);
+    const setLampColor = async (color: any) => {
+        setIsLoading(true);
+        const response = await fetch(`${API_BASE_URL}/lumisenseai/set-color`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                goveeKey: goveeKey.trim(),
+                device: device,
+                color: color,
+            }),
+        });
+        if (!response.ok) {
+            console.error("Failed to fetch device: ", response.status);
+        }
+        const result = await response.json();
+        console.log(result);
+        setIsLoading(false);
+    };
     return (
         <div className="w-full space-y-4 px-4 py-2">
             <AlertDialogHeader className="w-full">
@@ -39,10 +84,23 @@ const ColorPickerDialog = () => {
             </AlertDialogHeader>
             <AlertDialogFooter className="mt-10 ">
                 <div className="w-full flex justify-between">
-                    <Button onClick={() => {}}>Select</Button>
+                    <Button
+                        onClick={() => {
+                            setLampColor(hexToRgb(color));
+                        }}
+                    >
+                        Select
+                    </Button>
                     <AlertDialogCancel>Cancel</AlertDialogCancel>
                 </div>
             </AlertDialogFooter>
+            {isLoading && (
+                <div className="fixed inset-0 z-50 bg-custom-gray/30  flex items-center justify-center">
+                    <div className="bg-white rounded-full p-5 shadow-lg">
+                        <LoadingSpinner />
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
